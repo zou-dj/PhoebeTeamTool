4dcb316e9ab0b6333f29890ed4152192da18ef05
zou.dj <zou.dj@neusoft.com>
2019-10-25 15:55:40 +0800

    请假管理系统

---
+++ manageTool/src/main/java/com/manageTool/ManageToolApplication.java
@@ -0,0 +1,13 @@
+package com.manageTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageToolApplication.class, args);
+	}
+
+}
---
+++ manageTool/src/main/java/com/manageTool/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.manageTool.controller;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping(value = "/login")
+	public String login() {
+		
+		return "index";
+	}
+}
