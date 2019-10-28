64971497bf7372c705848b661152ef24301b7255
zou.dj <zou.dj@neusoft.com>
2019-10-28 14:16:02 +0800

    项目名变更

---
+++ holiday-manage-system/src/test/java/HolidayManageSystem/HolidayManageSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package HolidayManageSystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HolidayManageSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
